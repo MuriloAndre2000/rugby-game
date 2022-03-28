29b7a0ae347e1d46c1d01c9cc57d8f955ac9b6c1
Murilo André <40254653+MuriloAndre2000@users.noreply.github.com>
2022-03-27 23:42:41 -0300

    UPDate defender

--- src/defender.c
+++ src/defender.c
@@ -32,6 +32,19 @@ direction_t get_next_dir_random_deffender(
 
 }
 
+direction_t change_next_move_if_needed(
+	direction_t next_move, 
+	Spy attacker_spy,
+	position_t defender_position
+	){
+	position_t attacker_position = get_spy_position(attacker_spy);
+	if (defender_position.j != attacker_position.j){
+		if (attacker_position.j > defender_position.j)
+			return (direction_t) DIR_DOWN_LEFT;
+		return (direction_t) DIR_UP_LEFT;
+	}
+    return next_move;
+}
 
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
@@ -41,9 +54,9 @@ direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
   // TODO: unused parameters, remove these lines later
   // Global state
-	UNUSED(attacker_spy);
   static position_t last_pos = INVALID_POSITION;
   static direction_t last_move = DIR_STAY;
+  static int cheat_spy = 0;
   direction_t next_move = DIR_LEFT;
 
   if (equal_positions(last_pos, (position_t) INVALID_POSITION)) 
@@ -57,7 +70,12 @@ direction_t execute_defender_strategy(
   else {
   		next_move = get_next_dir_random_deffender((direction_t) DIR_LEFT, (direction_t) DIR_UP_LEFT, (direction_t) DIR_DOWN_LEFT);
   }
-
+  srand(time(NULL));
+  int random_number = rand()%5;
+  if (random_number%5 == 0 && cheat_spy == 0){
+	  next_move = change_next_move_if_needed(next_move, attacker_spy, defender_position);
+	  cheat_spy += 1;
+  }
   last_move = next_move;
   last_pos =  defender_position;
   // TODO: Implement Defender logic here
