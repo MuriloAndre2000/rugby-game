bea91bfd61eee4fa2f66c06a69df43e5e5d408bc
Murilo André <40254653+MuriloAndre2000@users.noreply.github.com>
2022-03-27 22:09:13 -0300

    Initial project

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
@@ -13,17 +15,52 @@
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
 /*----------------------------------------------------------------------------*/
+/*                              PRIVATE FUNCTIONS                              */
+/*----------------------------------------------------------------------------*/
+
+direction_t get_next_dir_random(
+	direction_t possible_direction_1, 
+	direction_t possible_direction_2,
+	direction_t possible_direction_3){
+    srand(time(NULL));
+    int random_number = rand()%3;
+  	if (random_number == 0)
+  		return possible_direction_1;
+  	if (random_number == 1)
+  		return possible_direction_2;
+  	return possible_direction_3;
+
+}
+
+/*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
+  //UNUSED(attacker_position);
   UNUSED(defender_spy);
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  // Global state
+  static position_t last_pos = INVALID_POSITION;
+  static direction_t last_move = DIR_STAY;
+  direction_t next_move = DIR_RIGHT;
+
+  if (equal_positions(last_pos, (position_t) INVALID_POSITION)) 
+    last_pos = attacker_position;
+  else if (equal_positions(last_pos, attacker_position)){ // Check if player did not move
+  	next_move = get_next_dir_random((direction_t) DIR_RIGHT, (direction_t) DIR_UP_RIGHT, (direction_t) DIR_DOWN_RIGHT);
+  	while (next_move.i == last_move.i){
+  		next_move = get_next_dir_random((direction_t) DIR_RIGHT, (direction_t) DIR_UP_RIGHT, (direction_t) DIR_DOWN_RIGHT);
+  	}
+  }
+  else {
+  		next_move = get_next_dir_random((direction_t) DIR_RIGHT, (direction_t) DIR_UP_RIGHT, (direction_t) DIR_DOWN_RIGHT);
+  }
+
+  last_move = next_move;
+  last_pos =  attacker_position;
+  return next_move;
 }
 
-/*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
@@ -13,15 +15,51 @@
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
 /*----------------------------------------------------------------------------*/
+/*                              PRIVATE FUNCTIONS                              */
+/*----------------------------------------------------------------------------*/
+
+direction_t get_next_dir_random_deffender(
+	direction_t possible_direction_1, 
+	direction_t possible_direction_2,
+	direction_t possible_direction_3){
+    srand(time(NULL));
+    int random_number = rand()%3;
+  	if (random_number == 0)
+  		return possible_direction_1;
+  	if (random_number == 1)
+  		return possible_direction_2;
+  	return possible_direction_3;
+
+}
+
+
+/*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
+  // Global state
+	UNUSED(attacker_spy);
+  static position_t last_pos = INVALID_POSITION;
+  static direction_t last_move = DIR_STAY;
+  direction_t next_move = DIR_LEFT;
 
+  if (equal_positions(last_pos, (position_t) INVALID_POSITION)) 
+    last_pos = defender_position;
+  else if (equal_positions(last_pos, defender_position)){ // Check if player did not move
+  	next_move = get_next_dir_random_deffender((direction_t) DIR_LEFT, (direction_t) DIR_UP_LEFT, (direction_t) DIR_DOWN_LEFT);
+  	while (next_move.i == last_move.i){
+  		next_move = get_next_dir_random_deffender((direction_t) DIR_LEFT, (direction_t) DIR_UP_LEFT, (direction_t) DIR_DOWN_LEFT);
+  	}
+  }
+  else {
+  		next_move = get_next_dir_random_deffender((direction_t) DIR_LEFT, (direction_t) DIR_UP_LEFT, (direction_t) DIR_DOWN_LEFT);
+  }
+
+  last_move = next_move;
+  last_pos =  defender_position;
   // TODO: Implement Defender logic here
   return (direction_t) DIR_LEFT;
 }
